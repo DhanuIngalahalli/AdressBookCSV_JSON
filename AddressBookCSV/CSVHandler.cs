53bfd019a17ba062e937062b5d9418416a706402
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-10-13 07:17:54 -0700

    Welcome to Addressbook using csv

---
+++ AddressBookCSV/CSVHandler.cs
@@ -0,0 +1,47 @@
+﻿using CsvHelper;
+using System;
+using CsvHelper;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AddressBookCSV
+{
+    class CSVHandler
+    {
+        string importFilePath = @"E:\BridgeLab\AddressBookCSV\AddressBook.txt";
+
+        public void WriteToCsv(Dictionary<string, AddressBookBuider> addressbookDictionary)
+        {
+            using (StreamWriter writer = new StreamWriter(importFilePath))
+            {
+                using (CsvWriter csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    foreach (AddressBookBuider item in addressbookDictionary.Values)
+                    {
+                        List<Contacts> contactRecord = item.addressBook.Values.ToList();
+                        csv.WriteRecords(contactRecord);
+                    }
+                }
+            }
+        }
+        public void ReadFromCSV()
+        {
+            using (StreamReader reader = new StreamReader(importFilePath))
+            {
+                using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    Console.WriteLine("Below are Contents of CSV File");
+                    List<Contacts> contactRecord = csv.GetRecords<Contacts>().ToList();
+                    foreach (Contacts contact in contactRecord)
+                    {
+                        Console.WriteLine(contact.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ AddressBookCSV/ReadWriteFileIO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AddressBookCSV
+{
+    class ReadWriteFileIO
+    {
+        static string file = @"E:\BridgeLab\AddressBookCSV\AddressBook.txt";
+
+        public void WriteToFile(Dictionary<string, AddressBookBuider> addressBookDictionary)
+        {
+            StreamWriter writer = new StreamWriter(file, true);
+            foreach (AddressBookBuider item in addressBookDictionary.Values)
+            {
+                foreach (Contacts contact in item.addressBook.Values)
+                {
+                    writer.WriteLine(contact.ToString());
+                }
+            }
+            Console.WriteLine("\nSuccessfully added to Text file.");
+            writer.Close();
+        }
+        public void ReadFromFile()
+        {
+            Console.WriteLine(File.ReadAllText(file));
+        }
+    }
+}
+
